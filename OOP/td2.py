c379cfdb53dab2cf288e36ed43cfeb8788d359d0
lucillegimenes <62749866+lucillegimenes@users.noreply.github.com>
2020-04-08 17:33:57 +0200

    Update td2.py

--- OOP/td2.py
+++ OOP/td2.py
@@ -1,31 +1,34 @@
+
 class Person:
   def __init__(self,name,age,nationality):
     self.name=name
     self.age=age
     self.nationality=nationality
 class Child(Person):
-  def __init__(self, school, favorite_color):
+  def __init__(self,school,favorite_color):
     self.school=school
     self.favorite_color=favorite_color
-  def school(self):
+  def whichSchool(self):
     print("I go to",self.school)
-  def presentation(self)
-    print("Hello my name is",self.name,"I'm",self.age,"years old and my favorite color is",self.favorite_color) 
+  def presentation(self):
+    print("My favorite color is",self.favorite_color) 
   
 class Adult(Person):
-  def __init__(self, job, bank_account):
+  def __init__(self,job,nb_children):
     self.job=job
-    self.bank_account=bank_account
-  def working(self)
-    print("I work as a",self.job)
-  def money(self)
-    print("My bank account is",bank_account)
+    self.nb_children=nb_children
+  def working(self):
+    print("I work as a ",self.job)
+  def children(self):
+      print("I have",self.nb_children,"children")
     
-#main
+
 if __name__ == '__main__':
-  Robert=Adult.Person('Robert',40,'American')
-  Leo=Child.Person('Leo',7,'British')
-  Robert.working
-  Robert.money
-  Leo.school
-  Leo.presentation
+  Robert=Person("Robert",40,'American')
+  Leo=Person('Leo',7,'British')
+  Robert=Adult("Teacher",3)
+  Leo=Child("The Great School", "blue")
+  Robert.working()
+  Robert.children()
+  Leo.presentation()
+  Leo.whichSchool()
